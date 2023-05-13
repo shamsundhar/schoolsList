e3214a64725ed71cfc986dd27e7697a919b4baf2
shyamyammanuru <shyam.kdp@gmail.com>
2023-05-13 23:50:46 +1200

    Initial Commit

--- app/src/main/java/com/shyam/schoolslist/ui/main/SchoolsListFragment.kt
+++ app/src/main/java/com/shyam/schoolslist/ui/main/SchoolsListFragment.kt
@@ -6,10 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.shyam.schoolslist.R
+import com.shyam.schoolslist.databinding.FragmentSchoolsListBinding
 
 class SchoolsListFragment : Fragment() {
     private val viewModel by viewModels<SchoolsListViewModel>()
+    private lateinit var binding: FragmentSchoolsListBinding
     companion object {
         fun newInstance() = SchoolsListFragment()
     }
@@ -22,7 +23,11 @@ class SchoolsListFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_schools_list, container, false)
+    ): View {
+        binding = FragmentSchoolsListBinding.inflate(inflater, container, false)
+
+
+
+        return binding.root
     }
 }

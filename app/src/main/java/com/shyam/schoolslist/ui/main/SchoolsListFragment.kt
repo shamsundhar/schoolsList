148ad01678a642ae267962121b1192420cc9422f
shyamyammanuru <shyam.kdp@gmail.com>
2023-05-15 22:04:49 +1200

    Added Loading Progress Bar

--- app/src/main/java/com/shyam/schoolslist/ui/main/SchoolsListFragment.kt
+++ app/src/main/java/com/shyam/schoolslist/ui/main/SchoolsListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -39,19 +40,19 @@ class SchoolsListFragment : Fragment() {
                 viewModel.uiState.collect {
                     when (it) {
                         is UiState.Success -> {
-//                            binding.progressBar.visibility = View.GONE
+                            binding.progressBar.visibility = View.GONE
                             renderList(it.data)
                             binding.schoolListRecyclerView.visibility = View.VISIBLE
                         }
                         is UiState.Loading -> {
-//                            binding.progressBar.visibility = View.VISIBLE
+                            binding.progressBar.visibility = View.VISIBLE
                             binding.schoolListRecyclerView.visibility = View.GONE
                         }
                         is UiState.Error -> {
                             //Handle Error
-//                            binding.progressBar.visibility = View.GONE
-//                            Toast.makeText(this@TopHeadlineActivity, it.message, Toast.LENGTH_LONG)
-//                                .show()
+                            binding.progressBar.visibility = View.GONE
+                            Toast.makeText(activity, it.message, Toast.LENGTH_LONG)
+                                .show()
                         }
                     }
                 }
